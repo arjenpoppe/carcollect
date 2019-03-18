1f6f33cee5bd73522b3c6920dc2e27ad78ba3cd8
Arjen Poppe <arjen_poppe@live.nl>
2019-03-18 12:17:31 +0100

    add simple fft example add env public .env file

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+export FLASK_APP=carcollect
+export FLASK_ENV=development
+flask run
--- carcollect/analyze.py
+++ carcollect/analyze.py
@@ -2,16 +2,20 @@ import functools
 import os
 
 from flask import (
-    Blueprint, flash, redirect, render_template, request, url_for, current_app
+    Blueprint, flash, redirect, render_template, request, url_for, current_app, send_from_directory
 )
 from werkzeug.utils import secure_filename
 
+import matplotlib.pyplot as plt
+from scipy.io import wavfile as wav
+from scipy.fftpack import fft
+import numpy as np
 
 from carcollect.db import get_db
 
 bp = Blueprint('analyze', __name__, url_prefix='/analyze')
 
-ALLOWED_EXTENSIONS = set(['mp3'])
+ALLOWED_EXTENSIONS = set(['mp3', 'wav'])
 
 
 def allowed_file(filename):
@@ -36,5 +40,19 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
             flash('file {} uploaded successfully'.format(filename))
-            return redirect(url_for('analyze.upload_file'))
+            return redirect(url_for('analyze.uploaded_file', filename=filename))
     return render_template('analyze/upload_file.html')
+
+
+@bp.route('/uploads/<filename>', methods=('GET', 'POST'))
+def uploaded_file(filename):
+    return render_template('analyze/uploads.html', filename=filename)
+
+
+@bp.route('/fft_test', methods=('GET', 'POST'))
+def fft_test():
+    rate, data = wav.read('{}/test.wav'.format(current_app.config['UPLOAD_FOLDER']))
+    fft_out = fft(data)
+
+    plt.plot(data, np.abs(fft_out))
+    plt.show()
