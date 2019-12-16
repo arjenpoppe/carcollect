796c25bfc9464773760e3d072555214588b3cca1
arjenpoppe <arjen_poppe@live.nl>
2019-12-16 08:31:36 +0100

    Tests and refactoring

--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -1,10 +1,29 @@
-def test_convert_mp3(client):
+def test_page_renders(client):
 	response = client.get('/analyze/upload')
 	assert response.status_code == 200
+
+	response = client.get('/analyze/uploads')
+	assert response.status_code == 200
+
 
 def test_analyze_file(client):
 	pass
 
+
+def test_file_conversion(client, app):
+	pass
+
+
+def test_read_file(client):
+	pass
+
+
+def test_create_plot_data(client):
+	pass
+
+
+def test_non_audio_file(client):
+	pass
 
 
 
--- tests/test_filemanager.py
+++ tests/test_filemanager.py
@@ -8,79 +8,79 @@ from werkzeug.datastructures import FileStorage
 
 
 def test_upload(client, app):
-	test_filename = 'test_file.txt'
-	message = f'file {test_filename} uploaded successfully'
-	encoded_message = message.encode('utf-8')
-	data = create_testfile(test_filename.split('.')[0], test_filename.split('.')[1])
-	
-	response = client.get('/files/upload')
-	assert response.status_code == 200
-	
-	response = client.post('/files/upload', data=data) 
-	assert encoded_message in response.data
+    test_filename = 'test_file.txt'
+    message = f'file {test_filename} uploaded successfully'
+    encoded_message = message.encode('utf-8')
+    data = create_testfile(test_filename.split('.')[0], test_filename.split('.')[1])
 
-	with app.app_context():
-		path = os.path.join(current_app.config['UPLOAD_FOLDER'], test_filename)
-		assert os.path.exists(path)
-		os.remove(path)
+    response = client.get('/files/upload')
+    assert response.status_code == 200
+
+    response = client.post('/files/upload', data=data)
+    assert encoded_message in response.data
+
+    with app.app_context():
+        path = os.path.join(current_app.config['UPLOAD_FOLDER'], test_filename)
+        assert os.path.exists(path)
+        os.remove(path)
 
 
 @pytest.mark.parametrize(('name', 'extension', 'message'), (
-    ('test', 'txt', b'File type not allowed'),
-    ('', '', b'No file found'),
-    ('test', 'wav', b'file test.wav uploaded successfully')
+        ('test', 'txt', b'File type not allowed'),
+        ('', '', b'No file found'),
+        ('test', 'wav', b'file test.wav uploaded successfully')
 ))
 def test_upload_errors(client, app, monkeypatch, name, extension, message):
-	data = create_testfile(name, extension)
+    data = create_testfile(name, extension)
 
-	class Recorder(object):
-		called = False
+    class Recorder(object):
+        called = False
 
-	def fake_call():
-		Recorder.called = True
+    def fake_call():
+        Recorder.called = True
 
-	# monkeypatch.setattr('carcollect.filemanager.upload', fake_call) 
-	
-	with app.app_context():
-		full_filename = f'{name}.{extension}'
-		path = os.path.join(current_app.config['UPLOAD_FOLDER'], 'analyze', full_filename)
+    # monkeypatch.setattr('carcollect.filemanager.upload', fake_call)
 
-		response = client.post('/analyze/upload', data=data, follow_redirects=True)
-		
-		assert message in response.data
+    with app.app_context():
+        full_filename = f'{name}.{extension}'
+        path = os.path.join(current_app.config['UPLOAD_FOLDER'], 'analyze', full_filename)
 
-		# remove testfile
-		if not full_filename == '.':	
-			if os.path.exists(path):
-				os.remove(path)
+        response = client.post('/analyze/upload', data=data, follow_redirects=True)
+
+        assert message in response.data
+
+        # remove testfile
+        if not full_filename == '.':
+            if os.path.exists(path):
+                os.remove(path)
 
 
 def test_filelist(client, testaccount):
-	# test redirect to login page
-	response = client.get('/files/')
-	assert response.status_code == 302
+    # test redirect to login page
+    response = client.get('/files/')
+    assert response.status_code == 302
 
-	#test page after logging in
-	testaccount.login()
-	response = client.get('/files/')
-	assert response.status_code == 200
+    # test page after logging in
+    testaccount.login()
+    response = client.get('/files/')
+    assert response.status_code == 200
 
 
 @pytest.mark.parametrize('filename', (
-    '.gitignore',
-    'analyze',
+        '.gitignore',
+        'analyze',
 ))
 def test_file_page(client, testaccount, filename):
-	testaccount.login()
-	url = f'/files/{filename}'
+    testaccount.login()
+    url = f'/files/{filename}'
 
-	response = client.get(url)
-	assert response.status_code == 200
+    response = client.get(url)
+    assert response.status_code == 200
 
 
 def create_testfile(name='', extension=''):
-	test_filename = ''
-	if not name == '' and not extension == '':
-		test_filename = f'{name}.{extension}'
-	data = {'file': (BytesIO(b'my file contents'), test_filename),}
-	return data
+    test_filename = ''
+    if not name == '' and not extension == '':
+        test_filename = f'{name}.{extension}'
+    data = {'file': (BytesIO(b'my file contents'), test_filename), }
+    return data
